b00450a649cf87c32c11fe9367e157289db5bc82
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-10-18 19:24:59 +0200

    Envío entero mejorado

--- UT1/envíoEntero.c
+++ UT1/envíoEntero.c
@@ -7,7 +7,7 @@ int main(void )
 {
         int fd[2];
         pid_t pid;
-        int entero = 42;
+        int entero = -1;
         char buffer[sizeof(entero)];
 
         pipe(fd); //creo pipe
@@ -19,12 +19,12 @@ int main(void )
                         exit (-1);
                 case 0: //HIJO RECIBE
                         close(fd[1]); //cierra el descriptor de entrada
-                        entero = -1;
                         read(fd[0], &entero, sizeof(entero)); //leo el pipe
-                        printf("\tEl HIJO recibe algo del pipe : %d\n",entero);
+                        printf("\tEl HIJO recibe algo del pipe : %d\n", entero);
                         break;
                 default : // PADRE ENVIA
                         close(fd[0]);
+                        entero = 42; // Número para enviar
                         printf("El PADRE ENVIA MENSAJE AL HIJO ...\n");
                         write(fd[1],&entero, sizeof(entero)); //escribo en pipe
                         wait(NULL); //espero al proceso hijo
